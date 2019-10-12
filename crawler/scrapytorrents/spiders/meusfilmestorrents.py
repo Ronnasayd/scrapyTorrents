a166608be7a785165118b2155581deaa8d9993f5
Ronnasayd Machado <ronnasayd@hotmail.com>
2019-10-12 20:45:21 -0300

    last fixes

--- crawler/scrapytorrents/spiders/meusfilmestorrents.py
+++ crawler/scrapytorrents/spiders/meusfilmestorrents.py
@@ -6,35 +6,36 @@ from bs4 import BeautifulSoup
 
 
 class MeusfilmestorrentsSpider(scrapy.Spider):
-    name = 'meusfilmestorrents'
-    start_urls = ['https://meusfilmestorrents.com']
+    name = "meusfilmestorrents"
+    start_urls = ["https://meusfilmestorrents.com"]
 
     def parse(self, response):
-        movies = response.css('a.more-link::attr(href)').getall()
+        movies = response.css("a.more-link::attr(href)").getall()
         for movie in movies:
             yield response.follow(movie, callback=self.getData)
         next_page = response.css(
-            '#content > div > div > a.nextpostslink::attr(href)').get()
+            "#content > div > div > a.nextpostslink::attr(href)"
+        ).get()
         yield response.follow(next_page, callback=self.parse)
 
     def getData(self, response):
-        info = response.css(
-            'div.entry-content.cf').get()
+        info = response.css("div.entry-content.cf").get()
         infoText = BeautifulSoup(info).get_text()
         infoDictionay = handleParserInfo(self, infoText)
         torrent = ScrapytorrentsItem(**infoDictionay)
 
-        torrent['image_url'] = response.css(
-            'div.entry-content.cf img::attr(src)').get(default="N/F")
-        torrent['trailer'] = response.css(
-            'iframe::attr(src)').get(default="N/A")
-        torrent['original_link'] = response.url
+        torrent["image_url"] = response.css("div.entry-content.cf img::attr(src)").get(
+            default="N/F"
+        )
+        torrent["trailer"] = response.css("iframe::attr(src)").get(default="N/A")
+        torrent["original_link"] = response.url
 
         magnet_links = response.css(
-            'div.entry-content.cf a[href^="magnet"]::attr(href)').getall()
-        torrent['magnet_links'] = handleParseMagnetLinks(self, magnet_links)
-        torrent['tipo'] = 'serie' if 'temporada' in response.url.lower() else 'filme'
-        torrent['titulo_slug'] = torrent['titulo'].lower().replace(' ', '_')
+            'div.entry-content.cf a[href^="magnet"]::attr(href)'
+        ).getall()
+        torrent["magnet_links"] = handleParseMagnetLinks(self, magnet_links)
+        torrent["tipo"] = "serie" if "temporada" in response.url.lower() else "filme"
+        torrent["titulo_slug"] = torrent["titulo"].lower().replace(" ", "_")
 
         self.logger.info(response.url)
         yield torrent
